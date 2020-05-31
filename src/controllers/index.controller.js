eabf98be8289f8f1c19c6f8f30bd714a2bf37842
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-05-31 17:19:20 -0300

    tried adding single quotes around attribute

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -28,7 +28,7 @@ const getUserById = async (req, res) => {
 }
 
 const getUserByUsername = async (req, res) => {
-    const response = await pool.query("SELECT * FROM users WHERE username = $1", [
+    const response = await pool.query("SELECT * FROM users WHERE username = '$1'", [
         req.body.username
     ]);
     res.status(200).json({response: response.rows});
