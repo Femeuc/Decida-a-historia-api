a5ddf041b25351759cfd83c162ad2f65b81f4df5
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-05-31 17:12:16 -0300

    tried another strategy

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -28,9 +28,8 @@ const getUserById = async (req, res) => {
 }
 
 const getUserByUsername = async (req, res) => {
-    const username = req.query.username;
     const response = await pool.query("SELECT * FROM users WHERE username = $1", [
-        username
+        req.body.username
     ]);
     res.status(200).json({response: response.rows});
 }
